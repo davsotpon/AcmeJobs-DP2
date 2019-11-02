1e75a76ed6bdbc36e0c46a39098bb69633e4d5bd
josec <josec@DESKTOP-9766SH7>
2019-11-02 15:04:30 +0100

    NO ME TOQUEIS EL NAVO A VER QUIEN TIENE HUEVOS DE ARREGLARLOP

--- src/main/java/acme/entities/records/Records.java
+++ src/main/java/acme/entities/records/Records.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Transient;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.URL;
@@ -21,6 +22,7 @@ public class Records extends DomainEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
+	@NotNull
 	@Transient
 	public String				company;
 
@@ -37,12 +39,15 @@ public class Records extends DomainEntity {
 	private String				web;
 
 	//@Pattern(regexp = "^[+]{0,1}[1-9]{0,2}[\\s]{0,1}[(]{0,1}[1-9]{0,4}[)]{0,1}[\\s]{0,1}[0-9]{5,9}")
+
+	// HASTA EL NAVO DE TO ESTE TAMBIEN DA FALLO Y ES EL DE MIGUEL@Pattern(regexp = "^([+]{1}[1-9][0-9]{0,2})?[(]{1}[0-9]{1,4}[)]{1}[0-9]{6,10}$")
 	private Integer				phone;
 
 	@Email
 	@NotBlank
 	private String				email;
 
+	@NotNull
 	public Boolean				incorporated;
 
 	@Range(min = 0, max = 5)
@@ -50,16 +55,18 @@ public class Records extends DomainEntity {
 
 
 	@Transient
-	public String getIncor() {
+	public String getCompanyIncorporated() {
 
 		StringBuilder res = new StringBuilder();
 		res.append(this.company);
 
 		if (this.incorporated == true) {
-			res.append("Inc.");
+
+			res.append(" Inc.");
 
 		} else {
-			res.append("LLC");
+
+			res.append(" LLC");
 		}
 
 		return res.toString();
--- src/main/java/acme/features/anonymous/records/AnonymousRecordsShowService.java
+++ src/main/java/acme/features/anonymous/records/AnonymousRecordsShowService.java
@@ -34,7 +34,7 @@ public class AnonymousRecordsShowService implements AbstractShowService<Anonymou
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "company", "sector", "CEO", "activities", "web", "phone", "email", "stars", "incorporated");
+		request.unbind(entity, model, "companyIncorporated", "sector", "CEO", "activities", "web", "phone", "email", "stars", "incorporated");
 
 	}
 

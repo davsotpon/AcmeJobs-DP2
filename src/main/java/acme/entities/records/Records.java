b2d9b6b558941cde241ebf885d80ca760e906aea
davsotpon <davsotpon@alum.us.es>
2019-11-05 13:50:00 +0100

    model

--- src/main/java/acme/entities/records/Records.java
+++ src/main/java/acme/entities/records/Records.java
@@ -37,6 +37,7 @@ public class Records extends DomainEntity {
 	@URL
 	private String				web;
 
+	@NotBlank
 	@Pattern(regexp = "^([+][\\d]{0,3})?[ ]?([(][\\d]{0,4}[)])?[ ]?([\\d]{6,10}|[\\d]{3} [\\d]{2} [\\d]{2} [\\d]{2}|[\\d]{3} [\\d]{3} [\\d]{3})$")
 	private String				phone;
 
